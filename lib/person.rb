f17714ff6a65423c7d244f78bd2b9ccfef0b9f3f
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-10-25 14:18:19 +0300

    completed Assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,32 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account
+    attr_accessor :name, :happiness,:hygiene
+    def initialize(name)
+        @name = name
+        @bank_account =25
+        @happiness=8
+        @hygiene=8
+    end
+    def happiness=(value)
+        @happiness=if value > 10
+            10
+        elsif value < 0
+            0
+        else
+            value
+        end
+    end
+    def hygiene=(value)
+        @hygiene = if value > 10
+            10
+    
+        elsif value < 0
+            0
+        else
+            value
+        end
+    end
+end
+mike=Person.new
+puts mike.hygiene(12)
